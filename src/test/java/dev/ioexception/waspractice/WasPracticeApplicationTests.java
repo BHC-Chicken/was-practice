ec22e2a6e6ce477992640db5cd067feb5e12f515
BHC-Chicken <tij05078@gmail.com>
2022-09-01 22:42:03 +0900

    Initial commit

---
+++ src/test/java/dev/ioexception/waspractice/WasPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ioexception.waspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
